163f781fcb7a7c22d79965ae2e904703744013a7
seanrose <seanrose@alumni.stanford.edu>
2014-08-17 13:01:37 -0700

    added functions for generating URLs

--- client/views/bottle.js
+++ client/views/bottle.js
@@ -1,3 +1,27 @@
+// after:2014/5/3 before:2014/5/4
+function historicalURL(date, yearsInThePast) {
+	// creating a copy because idk how to javascript tbh
+	var dateCopy = new Date(date);
+	var year = dateCopy.getFullYear() - yearsInThePast;
+
+	var afterComponent = 'after:' + historicalDate(dateCopy, yearsInThePast);
+
+	dateCopy.setDate(dateCopy.getDate() + 1);
+	var beforeComponent = 'before:' + historicalDate(dateCopy, yearsInThePast);
+
+	var baseURL = 'https://mail.google.com/mail/u/1/#search/';
+	var combinedURL = baseURL + encodeURIComponent(afterComponent) + '+' + encodeURIComponent(beforeComponent);
+
+	return combinedURL;
+}
+
+function historicalDate(date, yearsInThePast) {
+	var year = date.getFullYear() - yearsInThePast;
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	return year + '/' + month + '/' + day;
+}
+
 Template.bottle.helpers({
     historicalURLs: function() {
         return [
@@ -16,3 +40,7 @@ Template.bottle.helpers({
         ];
     }
 });
+
+
+var d = new Date();
+console.log(historicalURL(d, 1));
