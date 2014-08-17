efc692d7bbd01471ac2c9d3bfb78c2051a549c0d
seanrose <seanrose@alumni.stanford.edu>
2014-08-17 11:55:34 -0700

    hardcode all the things!

---
+++ client/views/bottle.js
@@ -0,0 +1,18 @@
+Template.bottle.helpers({
+    historicalURLs: function() {
+        return [
+        	{
+        		year: 1,
+        		url: 'https://mail.google.com/mail/u/1/#search/after%3A2013%2F8%2F17+before%3A2013%2F8%2F18'
+        	},
+        	{
+        		year: 2,
+        		url: 'https://mail.google.com/mail/u/1/#search/after%3A2012%2F8%2F17+before%3A2012%2F8%2F18'
+        	},
+        	{
+        		year: 3,
+        		url: 'https://mail.google.com/mail/u/1/#search/after%3A2011%2F8%2F17+before%3A2011%2F8%2F18'
+        	},
+        ];
+    }
+});
