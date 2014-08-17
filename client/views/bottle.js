e04a0c0a8a1f8291d06d88f8cb750a6eeee5ac92
seanrose <seanrose@alumni.stanford.edu>
2014-08-17 13:16:45 -0700

    works with hardcoded 4 years

--- client/views/bottle.js
+++ client/views/bottle.js
@@ -1,5 +1,5 @@
-// after:2014/5/3 before:2014/5/4
-function historicalURL(date, yearsInThePast) {
+// Format: after:2014/5/3 before:2014/5/4
+function historicalSearchURL(date, yearsInThePast) {
 	// creating a copy because idk how to javascript tbh
 	var dateCopy = new Date(date);
 	var year = dateCopy.getFullYear() - yearsInThePast;
@@ -24,23 +24,18 @@ function historicalDate(date, yearsInThePast) {
 
 Template.bottle.helpers({
     historicalURLs: function() {
-        return [
-        	{
-        		year: 1,
-        		url: 'https://mail.google.com/mail/u/1/#search/after%3A2013%2F8%2F17+before%3A2013%2F8%2F18'
-        	},
-        	{
-        		year: 2,
-        		url: 'https://mail.google.com/mail/u/1/#search/after%3A2012%2F8%2F17+before%3A2012%2F8%2F18'
-        	},
-        	{
-        		year: 3,
-        		url: 'https://mail.google.com/mail/u/1/#search/after%3A2011%2F8%2F17+before%3A2011%2F8%2F18'
-        	},
-        ];
+        var histories = [];
+        // TODO(seanrose): un-hardcode numYears
+        var numYears = 4;
+        var currentDate = new Date();
+
+        for (var i = numYears; i > 0; i--) {
+        	histories.push({
+        		text: i + ' ' + (i == 1 ? 'year' : 'years') + ' ago',
+        		url: historicalSearchURL(currentDate, i)
+        	});
+        }
+
+        return histories;
     }
 });
-
-
-var d = new Date();
-console.log(historicalURL(d, 1));
